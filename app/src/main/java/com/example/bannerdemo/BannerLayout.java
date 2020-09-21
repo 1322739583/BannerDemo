9a07f5e207e0dc6e2f2dc1b08a6c3b2566e86b6f
root <you@example.com>
2020-09-21 23:27:43 +0800

    添加自动轮播功能

--- app/src/main/java/com/example/bannerdemo/BannerLayout.java
+++ app/src/main/java/com/example/bannerdemo/BannerLayout.java
@@ -1,10 +1,18 @@
 package com.example.bannerdemo;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class BannerLayout extends ViewGroup {
 
@@ -14,18 +22,58 @@ public class BannerLayout extends ViewGroup {
     private int x;
     private int index;
 
+    private boolean isAuto=true;
+    private Timer timer = new Timer();
+    private TimerTask task;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            switch (msg.what) {
+                case 0:
+                    if (++index>=childCount){
+                        index=0;
+                    }
+                    scrollTo(index*childWidth,0);
+                    break;
+            }
+        }
+    };
+
     public BannerLayout(Context context) {
         super(context);
+        init();
     }
+
 
     public BannerLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public BannerLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
+    private void init() {
+        task = new TimerTask() {
+            @Override
+            public void run() {
+                if (isAuto) {
+                    handler.sendEmptyMessage(0);
+                }
+            }
+        };
+        timer.schedule(task, 100, 2000);
+    }
+
+    public void startAuto(){
+        isAuto=true;
+    }
+
+    public void stopAuto(){
+        isAuto=false;
+    }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -47,9 +95,9 @@ public class BannerLayout extends ViewGroup {
         if (changed) {
             for (int i = 0; i < childCount; i++) {
                 View childView = getChildAt(i);
-                int left=childWidth*i;
-                int right =left+childWidth;
-                int top=0;
+                int left = childWidth * i;
+                int right = left + childWidth;
+                int top = 0;
                 int bottom = top + childHeight;
                 childView.layout(left, top, right, bottom);
             }
@@ -65,23 +113,25 @@ public class BannerLayout extends ViewGroup {
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                x= (int) event.getX();
+                stopAuto();
+                x = (int) event.getX();
                 break;
             case MotionEvent.ACTION_MOVE:
-                int moveX= (int) event.getX();
-                int distance=moveX-x;
-                scrollBy(-distance,0);
-                x=moveX;
+                int moveX = (int) event.getX();
+                int distance = moveX - x;
+                scrollBy(-distance, 0);
+                x = moveX;
                 break;
             case MotionEvent.ACTION_UP:
-                int scrollX=getScrollX();
-                 index=(scrollX+childWidth/2)/childWidth;
-                if (index<0){
-                    index=0;
-                }else if (index>childCount-1){
-                    index=childCount-1;
+                int scrollX = getScrollX();
+                index = (scrollX + childWidth / 2) / childWidth;
+                if (index < 0) {
+                    index = 0;
+                } else if (index > childCount - 1) {
+                    index = childCount - 1;
                 }
-                scrollTo(childWidth*index,0);
+                scrollTo(childWidth * index, 0);
+                startAuto();
                 break;
             default:
                 break;
